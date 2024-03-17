b3144c97cf6e3bff59debc12768dc9be0a8e6017
dodier111 <65995124+dodier111@users.noreply.github.com>
2024-03-17 14:07:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Built done using python '
+    return 'Built doe using python '
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5000)
